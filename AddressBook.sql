3eff68b86ae373d60fd551b1749e9bb0069b79f0
pavanyalala <pavanyalala4508@gmail.com>
2021-09-04 01:46:28 +0530

    [pavan]:Address Book Table Creation

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,1 +1,16 @@
+/*UC-1*/
 Create Database AddressBookSystem
+
+/*UC-2*/
+Create Table AddressBook(
+ContactId int identity(1,1) Primary Key,
+FirstName varchar(20) not null,
+LastName varchar(20) not null,
+Address varchar(50) not null, 
+City varchar(20) not null,
+State varchar(20) not null,
+Zip bigint not null ,
+PhoneNumber bigint not null,
+Email varchar(20)not null
+)
+Select * From AddressBook
