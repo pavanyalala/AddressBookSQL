bec31a134d2a06d3ddb313b009ed106c81c69cb9
pavanyalala <pavanyalala4508@gmail.com>
2021-09-05 16:06:42 +0530

    [Pavan]:Apllied Normalization Technique to Address Book

--- AddressBook.sql
+++ AddressBook.sql
@@ -66,7 +66,56 @@ Insert into AddressBook values
 ('Funny', 'XYZ', 'Kothapet', 'Hyderabad', 'Telangana', '500085', '4444444444', 'funny@gmail', 'Friend AddressBook', 'Friend'),
 ('Honey', 'ZYX', 'Kokapet', 'Hyderabad', 'Telangana', '500036', '5555555555', 'honey@gmail', 'Family AddressBook', 'Family')
 
+/*UC-12*/
 
+Create Table ContactType
+(
+ContactTypeId int not null Identity(1,1) Primary key,
+ContactTypeName varchar(30) not null
+)
 
+Insert Into ContactType values
+('Family'),
+('Friend'),
+('Profession')
+
+select * from ContactType
+
+Create Table Address
+(
+AddressId int not null Identity(1,1) Primary key,
+Address varchar(50) not null,
+City varchar(20) not null,
+State varchar(20) not null,
+Zip bigint  not null
+)
+
+select * from Address
+
+Insert Into Address values
+('Dammaiguda','HYD','TS','500085'),
+('Nagole','HYD','TS','500068'),
+('Uppal','HYD','TS','500065')
+
+Create Table ContactDetails
+(
+ContactId int not null Identity(1,1) primary key,
+FirstName varchar(20) not null,
+LastName varchar(20) not null,
+PhoneNumber bigint not null,
+Email varchar(50) not null,
+AddressId int not null Foreign Key References Address(AddressId),
+ContactTypeId int not null Foreign key References ContactType(ContactTypeId)
+)
+
+Insert Into ContactDetails values
+('Bunny','ABC','9988774455','bunny@gmail,com','1','1'),
+('Sunny','BCD','1122334455','Sunny@gmail,com','2','2'),
+('Bunty','XYZ','4545454545','bunty@gmail,com','3','3')
+
+select * from ContactDetails
+
+Select * From ContactDetails cd, Address a, ContactType ct
+Where cd.AddressId=a.AddressId And cd.ContactTypeId=ct.ContactTypeId
 
 
